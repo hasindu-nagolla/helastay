923f8d11830260fc3d7d323196a8b6024d04e125
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-08-26 13:07:59 +0530

    starting the admin part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
 import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -15,13 +16,16 @@ const App = () => {
     <div>
       {!isOwnerPath && <Navbar />}
       {false && <HotelReg />}
-      
+
       <div className="mni-h-h[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
           <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/owner" element={<Layout/>}>
+
+          </Route>
         </Routes>
       </div>
       <Footer />
