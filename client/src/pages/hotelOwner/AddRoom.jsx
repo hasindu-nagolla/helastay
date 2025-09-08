627e45646c0c5988a858c5ec975840d526d32777
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-08 07:43:26 +0530

    ADDroom page

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import Title from "../../components/Title";
 
 const AddRoom = () => {
-  return (
-    <div>AddRoom</div>
-  )
-}
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: 0,
+    amenities: {
+      "Free WiFi": false,
+      "Free Breakfast": false,
+      "Room Service": false,
+      "Mountain View": false,
+      "Pool Access": false,
+    },
+  });
 
-export default AddRoom
+  return (
+    <form>
+      <Title
+        align="left"
+        font="outfit"
+        title="Add Rom"
+        subTitle="Welcome back to your Hotel Booking Dashboard – here you can manage reservations, 
+        monitor occupancy rates, track guest check-ins and check-outs, and explore insightful statistics 
+        about your properties across Sri Lanka, all in one convenient place."
+      />
+
+      {/* upload area for images */}
+      <p className="text-gray-800 mt-10">Images</p>
+      <div className="grid grid-cols-2 sm:flex gap-4 my-2 fles-wrap">
+        {Object.keys(images).map((key)=> (
+          <label htmlFor=""></label>
+        ))}
+      </div>
+    </form>
+  );
+};
+
+export default AddRoom;
