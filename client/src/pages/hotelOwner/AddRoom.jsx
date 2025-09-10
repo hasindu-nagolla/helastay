a2c09ad9c8793f5e8cb2b47bfacc2c2307adaf13
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-10 22:40:37 +0530

    ADDroom page

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const AddRoom = () => {
   const [images, setImages] = useState({
@@ -35,7 +36,9 @@ const AddRoom = () => {
       <p className="text-gray-800 mt-10">Images</p>
       <div className="grid grid-cols-2 sm:flex gap-4 my-2 fles-wrap">
         {Object.keys(images).map((key) => (
-          <label htmlFor=""></label>
+          <label htmlFor={`roomImage${key}`} key={key}>
+            <img className="max-h-13 cursor-pointer opacity-80 " src={images[key] ? URL.createObjectURL(images[key]) : assets.uploadArea} alt="" />
+          </label>
         ))}
       </div>
     </form>
