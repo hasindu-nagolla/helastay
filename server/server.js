99652b7ccc141f25843e98b62af9bdc6e45196db
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 14:38:56 +0530

    Fix Clerk webhook and MongoDB Atlas connection

--- server/server.js
+++ server/server.js
@@ -4,22 +4,20 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import getRawBody from "raw-body";
 
 connectDB();
-
 const app = express();
 app.use(cors());
 
-// Middleware
-app.use(express.json());
+// Only use on /api/clerk for webhooks!
+app.post("/api/clerk", async (req, res, next) => {
+  req.rawBody = await getRawBody(req);
+  next();
+}, express.json(), clerkWebhooks);
+
 app.use(clerkMiddleware());
-
-// API to listen clerk webhooks
-app.use("/api/clerk", clerkWebhooks);
-
-// Public route (no auth required)
 app.get("/", (req, res) => res.send("API is working..."));
-
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5007;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
