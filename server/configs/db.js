34502ebecf62bbf1f9ae37bd0bb55928967fd9ac
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 08:22:04 +0530

    clerk setup

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,11 +5,12 @@ const connectDB = async () => {
     mongoose.connection.on("connected", () =>
       console.log("Database Connected")
     );
-    await mongoose.connect(`${process.env.MONGO_URI}/hotel-booking`);
+    await mongoose.connect(process.env.MONGO_URI);
   } catch (error) {
     console.log(error.message);
   }
 };
 
 export default connectDB;
+
 
