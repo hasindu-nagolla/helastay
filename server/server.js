5b2b56b736dab0cf6c591029cbe9c478b8e90b37
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 08:33:45 +0530

    clerk setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,29 @@
+// import express from "express";
+// import "dotenv/config";
+// import cors from "cors";
+// import connectDB from "./configs/db.js";
+// import { clerkMiddleware } from "@clerk/express";
+// import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+
+// // Middleware
+// app.use(express.json());
+// app.use(clerkMiddleware());
+
+// // API to listen clerk webhooks
+// app.use("/api/clerk", clerkWebhooks);
+
+// // Public route (no auth required)
+// app.get("/", (req, res) => res.send("API is working..."));
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
@@ -6,20 +32,14 @@ import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
-
 const app = express();
 app.use(cors());
 
-// Middleware
-app.use(express.json());
+app.post("/api/clerk", express.raw({ type: "*/*" }), clerkWebhooks);
+
+
 app.use(clerkMiddleware());
-
-// API to listen clerk webhooks
-app.use("/api/clerk", clerkWebhooks);
-
-// Public route (no auth required)
 app.get("/", (req, res) => res.send("API is working..."));
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
