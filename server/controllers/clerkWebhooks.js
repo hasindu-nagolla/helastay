1b511a7c684ce6d0a9764009f6472d3804df5575
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 08:42:31 +0530

    clerk setup

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -58,9 +58,11 @@ const clerkWebhooks = async (req, res) => {
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     };
-    await whook.verify(req.rawBody, headers);
+    const payloadBuffer = req.body;
+    await whook.verify(payloadBuffer, headers);
 
-    const { data, type } = req.body;
+    const { data, type } = JSON.parse(payloadBuffer.toString());
+
     const userData = {
       _id: data.id,
       email: data.email_addresses[0].email_address,
@@ -90,6 +92,5 @@ const clerkWebhooks = async (req, res) => {
     res.status(400).json({ success: false, message: error.message });
   }
 };
-
 
 export default clerkWebhooks;
