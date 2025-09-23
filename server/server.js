1336950ce9e21474b98975aed453c93c3b6fd428
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 08:08:59 +0530

    clerk setup

--- server/server.js
+++ server/server.js
@@ -1,25 +1,48 @@
+// import express from "express";
+// import "dotenv/config";
+// import cors from "cors";
+// import connectDB from "./configs/db.js";
+// import { clerkMiddleware } from "@clerk/express";
+// import clerkWebhooks from "./controllers/clerkWebhooks.js";
+
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+
+// // Middleware
+// app.use(express.json());
+// app.use(clerkMiddleware());
+
+// // API to listen clerk webhooks
+// app.use("/api/clerk", clerkWebhooks);
+
+// // Public route (no auth required)
+// app.get("/", (req, res) => res.send("API is working..."));
+
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import getRawBody from "raw-body";
 
 connectDB();
-
 const app = express();
 app.use(cors());
 
-// Middleware
-app.use(express.json());
+app.post("/api/clerk", async (req, res, next) => {
+    req.rawBody = await getRawBody(req);
+    next();
+}, express.json(), clerkWebhooks);
+
 app.use(clerkMiddleware());
-
-// API to listen clerk webhooks
-app.use("/api/clerk", clerkWebhooks);
-
-// Public route (no auth required)
 app.get("/", (req, res) => res.send("API is working..."));
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
