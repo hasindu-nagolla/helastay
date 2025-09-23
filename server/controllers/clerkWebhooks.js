2dbb3e0eee8a5e338e700594543e071ee55bd975
hasindu lakshan <hasindu-nagolla@users.noreply.github.com>
2025-09-23 12:18:52 +0530

    Test

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -4,19 +4,16 @@ import { Webhook } from "svix";
 const clerkWebhooks = async (req, res) => {
   try {
     // create a Svix instance with clerk webhook secret
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET); // getting headers
 
-    // getting headers
     const headers = {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
-    };
+    }; // verifying the headers
 
-    // verifying the headers
-    await whook.verify(JSON.stringify(req.body), headers);
+    await whook.verify(JSON.stringify(req.body), headers); // getting data from req body
 
-    // getting data from req body
     const { data, type } = req.body;
 
     const userData = {
@@ -24,9 +21,8 @@ const clerkWebhooks = async (req, res) => {
       email: data.email_addresses[0].email_address,
       username: data.first_name + " " + data.last_name,
       image: data.image_url,
-    };
+    }; // switch case for different events
 
-    // switch case for different events
     switch (type) {
       case value:
       case "user.created": {
@@ -49,53 +45,5 @@ const clerkWebhooks = async (req, res) => {
   console.log(error.message);
   res.json({ success: false, message: error.message });
 };
-
-// const clerkWebhooks = async (req, res) => {
-//   try {
-//     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-//     const headers = {
-//       "svix-id": req.headers["svix-id"],
-//       "svix-timestamp": req.headers["svix-timestamp"],
-//       "svix-signature": req.headers["svix-signature"],
-//     };
-//     const payloadBuffer = req.body;
-//     await whook.verify(payloadBuffer, headers);
-
-//     const { data, type } = JSON.parse(payloadBuffer.toString());
-
-//     const userData = {
-//       _id: data.id,
-//       email: data.email_addresses[0].email_address,
-//       username: data.first_name + " " + data.last_name,
-//       image: data.image_url,
-//     };
-
-//     switch (type) {
-//       case "user.created": {
-//         await User.updateOne(
-//           { _id: userData._id },
-//           { $set: userData },
-//           { upsert: true }
-//         );
-
-//         break;
-//       }
-//       case "user.updated": {
-//         await User.findByIdAndUpdate(data.id, userData);
-//         break;
-//       }
-//       case "user.deleted": {
-//         await User.findByIdAndDelete(data.id);
-//         break;
-//       }
-//       default:
-//         break;
-//     }
-//     res.json({ success: true, message: "Webhook received" });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(400).json({ success: false, message: error.message });
-//   }
-// };
 
 export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -5,47 +5,21 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
-
 connectDB();
-
 
 const app = express();
 app.use(cors());
-
 
 // Middleware
 app.use(express.json());
 app.use(clerkMiddleware());
 
-
 // API to listen clerk webhooks
 app.use("/api/clerk", clerkWebhooks);
-
 
 // Public route (no auth required)
 app.get("/", (req, res) => res.send("API is working..."));
 
-
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// import express from "express";
-// import "dotenv/config";
-// import cors from "cors";
-// import connectDB from "./configs/db.js";
-// import { clerkMiddleware } from "@clerk/express";
-// import clerkWebhooks from "./controllers/clerkWebhooks.js";
-
-// connectDB();
-// const app = express();
-// app.use(cors());
-
-// app.post("/api/clerk", express.raw({ type: "*/*" }), clerkWebhooks);
-
-
-// app.use(clerkMiddleware());
-// app.get("/", (req, res) => res.send("API is working..."));
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
